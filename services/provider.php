e605febb7c77dee9029f7dd2eebcd5322b1aa426
trep-kalkyl <emil.appelkvist@gmail.com>
2025-09-03 15:08:49 +0200

    Update provider.php

--- services/provider.php
+++ services/provider.php
@@ -16,7 +16,7 @@ use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
-use Joomla\Plugin\Webservices\Helloworld\Extension\Helloworld;
+use Joomla\Plugin\WebServices\Helloworld\Extension\Helloworld;
 
 return new class () implements ServiceProviderInterface {
     /**
