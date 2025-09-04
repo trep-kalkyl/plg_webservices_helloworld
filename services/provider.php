8f7a9da965c1ded08333c49994d30680a8db552d
trep-kalkyl <emil.appelkvist@gmail.com>
2025-09-04 15:01:40 +0200

    Update Helloworld namespace in provider.php

--- services/provider.php
+++ services/provider.php
@@ -1,14 +1,9 @@
 <?php
 
-/**
- * @package     Joomla.Plugin
- * @subpackage  Webservices.Helloworld
- *
- * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 \defined('_JEXEC') or die;
+
+// DEBUG 1: SUCCESS!
+// die('DEBUG: provider.php is loaded.');
 
 use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Factory;
@@ -16,23 +11,20 @@ use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
-use Joomla\Plugin\WebServices\Helloworld\Extension\Helloworld;
+use TrepKalkyl\Plugin\Webservices\Helloworld\Extension\Helloworld;
 
 return new class () implements ServiceProviderInterface {
-    /**
-     * Registers the service provider with a DI container.
-     *
-     * @param   Container  $container  The DI container.
-     *
-     * @return  void
-     *
-     * @since   1.0.0
-     */
     public function register(Container $container): void
     {
+        // DEBUG 2: SUCCESS!
+        // die('DEBUG: register() method in provider.php is called.');
+
         $container->set(
             PluginInterface::class,
             function (Container $container) {
+                // DEBUG 3: SUCCESS!
+                // die('DEBUG: Helloworld object creation is attempted.');
+
                 $plugin = new Helloworld(
                     $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('webservices', 'helloworld')
