29a1c6c14e428b7596fdf4b822a0b460ebcf11f3
trep-kalkyl <emil.appelkvist@gmail.com>
2025-09-04 15:02:22 +0200

    Refactor Helloworld plugin namespace and routes

--- src/Extension/Helloworld.php
+++ src/Extension/Helloworld.php
@@ -1,18 +1,23 @@
 <?php
 
-namespace Joomla\Plugin\WebServices\Helloworld\Extension;
-
-use Joomla\CMS\Event\Application\BeforeApiRouteEvent;
-use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\Event\SubscriberInterface;
-use Joomla\Router\Route;
+namespace TrepKalkyl\Plugin\Webservices\Helloworld\Extension;
 
 \defined('_JEXEC') or die;
+
+use Joomla\CMS\Event\Application\BeforeApiRouteEvent;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Response\JsonResponse;
+use Joomla\Event\SubscriberInterface;
+use Joomla\Router\Route;
 
 final class Helloworld extends CMSPlugin implements SubscriberInterface
 {
     public static function getSubscribedEvents(): array
     {
+        // DEBUG 5: SUCCESS!
+        // die('DEBUG: getSubscribedEvents() is called.');
+
         return [
             'onBeforeApiRoute' => 'onBeforeApiRoute',
         ];
@@ -20,17 +25,27 @@ final class Helloworld extends CMSPlugin implements SubscriberInterface
 
     public function onBeforeApiRoute(BeforeApiRouteEvent $event): void
     {
+        // DEBUG 6: SUCCESS!
+        // die('DEBUG: onBeforeApiRoute() is called.');
+
         $router = $event->getRouter();
 
-        // Skapa en route som använder com_content's struktur
         $route = new Route(
             ['GET'],
             'v1/helloworld/ping',
-            'articles.displayList', // Använd befintlig controller
+            'helloworld.ping',
             [],
-            ['component' => 'com_content', 'view' => 'articles']
+            ['public' => true]
         );
 
         $router->addRoute($route);
+
+        // DEBUG 7: SUCCESS!
+        // die('DEBUG: Route has been added.');
+    }
+
+    public function ping(): JsonResponse
+    {
+        return new JsonResponse(['hello' => 'world']);
     }
 }
