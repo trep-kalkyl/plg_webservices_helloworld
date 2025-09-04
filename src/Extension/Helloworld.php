8d516a5a0fc928169d57b110f8a39bed51edf4af
trep-kalkyl <emil.appelkvist@gmail.com>
2025-09-04 07:00:13 +0200

    Refactor Helloworld plugin for API route registration

--- src/Extension/Helloworld.php
+++ src/Extension/Helloworld.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * @package     Joomla.Plugin
- * @subpackage  Webservices.Helloworld
- *
- * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 namespace Joomla\Plugin\WebServices\Helloworld\Extension;
 
 use Joomla\CMS\Event\Application\BeforeApiRouteEvent;
@@ -15,24 +7,10 @@ use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Event\SubscriberInterface;
 use Joomla\Router\Route;
 
-// phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
-// phpcs:enable PSR1.Files.SideEffects
 
-/**
- * Web Services adapter for Hello World plugin.
- *
- * @since  1.0.0
- */
 final class Helloworld extends CMSPlugin implements SubscriberInterface
 {
-    /**
-     * Returns an array of events this subscriber will listen to.
-     *
-     * @return  array
-     *
-     * @since   1.0.0
-     */
     public static function getSubscribedEvents(): array
     {
         return [
@@ -40,25 +18,20 @@ final class Helloworld extends CMSPlugin implements SubscriberInterface
         ];
     }
 
-    /**
-     * Registers Hello World API routes in the application
-     *
-     * @param   BeforeApiRouteEvent  $event  The event object
-     *
-     * @return  void
-     *
-     * @since   1.0.0
-     */
     public function onBeforeApiRoute(BeforeApiRouteEvent $event): void
     {
         $router = $event->getRouter();
 
+        // Registrera controllern först
+        $router->addNamespace('Joomla\\Plugin\\WebServices\\Helloworld\\Controller', JPATH_PLUGINS . '/webservices/helloworld/src/Controller');
+        
+        // Skapa route som pekar på din controller
         $route = new Route(
             ['GET'],
             'v1/helloworld/ping',
-            function () {
-                return ['hello' => 'world'];
-            }
+            'helloworld.ping',
+            [],
+            ['component' => 'com_plugins'] // Använd com_plugins som komponent
         );
 
         $router->addRoute($route);
