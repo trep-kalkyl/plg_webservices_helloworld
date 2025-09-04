17bd34dbfa0be31f99efa85f5e79068858f00cd5
trep-kalkyl <emil.appelkvist@gmail.com>
2025-09-04 06:46:42 +0200

    Update Helloworld.php to register API routes

--- src/Extension/Helloworld.php
+++ src/Extension/Helloworld.php
@@ -1,15 +1,38 @@
 <?php
 
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Webservices.Helloworld
+ *
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 namespace Joomla\Plugin\WebServices\Helloworld\Extension;
 
-use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Event\Application\BeforeApiRouteEvent;
+use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Event\SubscriberInterface;
+use Joomla\Router\Route;
 
+// phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
 
+/**
+ * Web Services adapter for Hello World plugin.
+ *
+ * @since  1.0.0
+ */
 final class Helloworld extends CMSPlugin implements SubscriberInterface
 {
+    /**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since   1.0.0
+     */
     public static function getSubscribedEvents(): array
     {
         return [
@@ -17,14 +40,27 @@ final class Helloworld extends CMSPlugin implements SubscriberInterface
         ];
     }
 
+    /**
+     * Registers Hello World API routes in the application
+     *
+     * @param   BeforeApiRouteEvent  $event  The event object
+     *
+     * @return  void
+     *
+     * @since   1.0.0
+     */
     public function onBeforeApiRoute(BeforeApiRouteEvent $event): void
     {
         $router = $event->getRouter();
-        $router->get(
-            'helloworld/ping',
+
+        $route = new Route(
+            ['GET'],
+            'v1/helloworld/ping',
             function () {
                 return ['hello' => 'world'];
             }
         );
+
+        $router->addRoute($route);
     }
 }
